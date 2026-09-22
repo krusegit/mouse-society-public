101aad4dbc7b01c2f8e1c1579bc98d483d87a394
kruseio <46799551+kruseio@users.noreply.github.com>
2026-09-22 07:37:50 +0000

    Deploy web build from 6d5263bad1248f06681c7b151c9976a9059d59cc

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790030778|1590526';
+const CACHE_VERSION = '1790062664|1433852';
 /** @type {string} */
 const CACHE_PREFIX = 'Mouse Society-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
